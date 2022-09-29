e43a1644646384232965da466ebf916c7ea3c763
Yaluhn <66998236+Yaluhn@users.noreply.github.com>
2022-09-28 22:27:56 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,131 @@
+import "./styles.css";
+import Header from "./components/header";
+import Section from "./components/Section";
+import ListItem from "./components/Listitem";
+
+const gamesListData = [
+  {
+    url: "https://www.twitch.tv/directory/game/League%20of%20Legends",
+    imageUrl: "https://static-cdn.jtvnw.net/ttv-boxart/21779-188x250.jpg",
+    alt: "Imagem do jogo League of Legends"
+  },
+
+  {
+    url: "https://www.twitch.tv/directory/game/VALORANT",
+    imageUrl: "https://static-cdn.jtvnw.net/ttv-boxart/516575-188x250.jpg",
+    alt: "Imagem do jogo Valorant"
+  },
+
+  {
+    url: "https://www.twitch.tv/directory/game/Minecraft",
+    imageUrl: "https://static-cdn.jtvnw.net/ttv-boxart/27471_IGDB-188x250.jpg",
+    alt: "Imagem do jogo Minecraft"
+  },
+
+  {
+    url: "https://www.twitch.tv/directory/game/Teamfight%20Tactics",
+    imageUrl: "https://static-cdn.jtvnw.net/ttv-boxart/513143-188x250.jpg",
+    alt: "Imagem do jogo TFT"
+  }
+];
+
+const channeListData = [
+  {
+    url: "https://www.twitch.tv/maykbrito",
+    imageUrl:
+      "https://static-cdn.jtvnw.net/jtv_user_pictures/9ce11a2b-ec84-44b1-9c76-b8d29df5fef0-profile_image-150x150.png",
+    alt: "Imagem de Mayk Brito"
+  },
+
+  {
+    url: "https://www.twitch.tv/alanzoka",
+    imageUrl:
+      "https://static-cdn.jtvnw.net/jtv_user_pictures/15cec952-c1ba-4ff8-a79c-53c2fa5bd269-profile_image-150x150.png",
+    alt: "Imagem de Alanzoka"
+  },
+  {
+    url: "https://www.twitch.tv/cellbit",
+    imageUrl:
+      "https://static-cdn.jtvnw.net/jtv_user_pictures/cb885149-f7f7-48f0-99ae-fe0a386ed3e2-profile_image-150x150.png",
+    alt: "Imagemd de Cellbit"
+  }
+];
+
+const socialListData = [
+  {
+    url: "http://instagram.com",
+    imageUrl: "/assets/instagram.svg",
+    alt: "acesse o instagram"
+  },
+  {
+    url: "http://twittwe.com",
+    imageUrl: "/assets/twitter.svg",
+    alt: "acesse o twitter"
+  },
+  {
+    url: "http://twitch.com",
+    imageUrl: "/assets/twitch.svg",
+    alt: "acesse o twitch"
+  },
+  {
+    url: "http://youtube.com",
+    imageUrl: "/assets/youtube.svg",
+    alt: "acesse o youtube"
+  }
+];
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <Section
+          title="Meus Jogos"
+          subtitle="Os Games que eu mais curto jogar!"
+          className="game-list"
+        >
+          {gamesListData.map(function (item) {
+            return (
+              <ListItem
+                url={item.url}
+                imageUrl={item.imageUrl}
+                alt={item.alt}
+              />
+            );
+          })}
+        </Section>
+        <Section
+          title="Canais e Streamers"
+          subtitle="Lista de Canais e transmissões que não perco"
+          className="channel-list"
+        >
+          {channeListData.map(function (item) {
+            return (
+              <ListItem
+                url={item.url}
+                imageUrl={item.imageUrl}
+                alt={item.alt}
+              />
+            );
+          })}
+        </Section>
+
+        <Section
+          title="Minhas Redes"
+          subtitle="Conecte-se comigo"
+          className="social-list"
+        >
+          {socialListData.map(function (item) {
+            return (
+              <ListItem
+                url={item.url}
+                imageUrl={item.imageUrl}
+                alt={item.alt}
+              />
+            );
+          })}
+        </Section>
+      </main>
+    </div>
+  );
+}
